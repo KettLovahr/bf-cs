a561691834079372496afcd37cff7350f90766e5
Kett Lovahr <catusfelony@gmail.com>
2025-07-03 15:35:32 -0300

    Setup tasks for building and running

--- build.sh
+++ build.sh
@@ -2,3 +2,10 @@
 
 mcs Raylib.cs BrainFuck.cs BFCli.cs -out:BFCli.exe
 mcs Raylib.cs BrainFuck.cs KettGui.cs BFGui.cs -out:BFGui.exe
+
+if [[ "$1" == "runcli" ]] then
+    mono BFCli.exe;
+fi
+if [[ "$1" == "rungui" ]] then
+    mono BFGui.exe;
+fi
